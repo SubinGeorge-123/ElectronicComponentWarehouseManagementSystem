8e10b125ce68330707313705d3a3332eabfa395e
SUBIN GEORGE <x24111945@student.ncirl.ie>
2025-11-11 11:26:43 +0000

    fix

--- ElectronicComponentWarehouseManagementSystemApp/settings.py
+++ ElectronicComponentWarehouseManagementSystemApp/settings.py
@@ -88,24 +88,7 @@ WSGI_APPLICATION = 'ElectronicComponentWarehouseManagementSystemApp.wsgi.applica
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# Database Configuration
-if 'RDS_HOSTNAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ.get('RDS_PORT', '3306'),
-            'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                'charset': 'utf8mb4',
-            }
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
